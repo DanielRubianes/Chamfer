ee7b31c2a778be58082da85d5581927fb2a371dc
DanielRubianes <DanielRubianes1352@gmail.com>
2024-10-27 13:02:10 -0400

    Cleanup, extend from point closest to theoretic intersection

--- Chamfer.cs
+++ Chamfer.cs
@@ -31,7 +31,7 @@ namespace Chamfer
         private IDisposable _graphic = null;
         private CIMLineSymbol _lineSymbol = null;
 
-        private ObservableCollection<Polyline> _selectedSegments = new();
+        private ObservableCollection<Polyline> _selected_segments = new();
 
         private static readonly object _lock = new object();
 
@@ -54,41 +54,69 @@ namespace Chamfer
             }
         }
 
-        // TODO: Implement esc handling to further mimick fillet behavior
+        // TODO: Implement esc handling to further mimic fillet behavior
         protected override Task<bool> OnSketchCompleteAsync(Geometry point_selection)
         {
             QueuedTask.Run(() =>
             {
-                // Get visually selected features from active map
-                var selected_features = MapView.Active.GetFeatures(point_selection, true);
-                if (selected_features.IsEmpty)
+                var selected_features = MapView.Active.GetFeatures(point_selection, true); // Get visually selected features from active map
+                if (selected_features.IsEmpty) // Exit if no features selected
                     return false;
 
                 var insp = new Inspector();
                 var features_oids = selected_features[selected_features.ToDictionary().Keys.First()];
                 insp.Load(selected_features.ToDictionary().First().Key, features_oids.First());
-                if (insp.HasAnnotationAttributes)
+                if (insp.HasAnnotationAttributes) // Exit if feature selected is annotation
                     return false;
 
-                Geometry selected_geom = insp.Shape;
-                Polyline filtered_geom = insp.Shape.GeometryType == GeometryType.Polygon ? GeometryEngine.Instance.Boundary(insp.Shape) as Polyline : insp.Shape as Polyline;
-                if (filtered_geom == null)
-                    return false;
+                Polyline outline_geom =
+                    insp.Shape.GeometryType == GeometryType.Polygon
+                    ? GeometryEngine.Instance.Boundary(insp.Shape) as Polyline
+                    : insp.Shape as Polyline;
 
-                ReadOnlyPartCollection segments = filtered_geom.Parts;
-                Segment selected_segment = segments
-                    .SelectMany(collection => collection).ToList()
+                if (outline_geom == null) // Exit if null or non-line geometry
+                    return false;
+                
+                // Find closest segment (polyline part) to the selection point
+                Segment selected_segment = outline_geom.Parts
+                    .SelectMany(segment => segment).ToList()
                     .OrderBy(segment => GeometryEngine.Instance.Distance(point_selection, PolylineBuilderEx.CreatePolyline(segment)))
                     .FirstOrDefault();
-
                 Polyline selected_seg_geom = PolylineBuilderEx.CreatePolyline(selected_segment);
 
-                // Case: none selected or two segments selected
-                // TOTO: separate these cases to make two selected the commit operation case
-                if (_selectedSegments.Count > 1 || _selectedSegments.Count == 0)
+                // Case: No selection made
+                if (_selected_segments.Count == 0)
                 {
-                    _selectedSegments.Clear();
-                    _selectedSegments.Add(selected_seg_geom);
+                    _selected_segments.Clear();
+                    _selected_segments.Add(selected_seg_geom);
+                    lock (_lock)
+                    {
+                        if (_graphic != null)
+                            _graphic.Dispose();
+                        _graphic = this.AddOverlay(selected_seg_geom, _lineSymbol.MakeSymbolReference());
+                    }
+                }
+                // Case: One segment already selected
+                else if (_selected_segments.Count == 1)
+                {
+                    Polyline extensions = ChamferLines(_selected_segments[0], selected_seg_geom);
+                    // Case: No intersection found (parallel lines)
+                    if (extensions == null)
+                        return false;
+                    _selected_segments.Add(selected_seg_geom);
+                    var merged_geoms = GeometryEngine.Instance.Union(_selected_segments[0], selected_seg_geom) as Polyline;
+                    lock (_lock)
+                    {
+                        //this.UpdateOverlay(_graphic, merged_geoms, _lineSymbol.MakeSymbolReference());
+                        this.UpdateOverlay(_graphic, extensions, _lineSymbol.MakeSymbolReference());
+                    }
+                }
+                // Case: Two segments already selected
+                // This is a stub to allow repeated testing of initial selection
+                else if (_selected_segments.Count > 1)
+                {
+                    _selected_segments.Clear();
+                    _selected_segments.Add(selected_seg_geom);
                     lock (_lock)
                     {
                         if (_graphic != null)
@@ -96,39 +124,6 @@ namespace Chamfer
 
                         _graphic = this.AddOverlay(selected_seg_geom, _lineSymbol.MakeSymbolReference());
                     }
-                }
-                // Case: one segment already selected
-                else if (_selectedSegments.Count == 1)
-                {
-                    MapPoint int_point = ChamferLines(_selectedSegments[0] as Polyline, selected_seg_geom as Polyline);
-                    // Case: No intersection found (parallel lines)
-                    if (int_point == null)
-                        return false;
-                    _selectedSegments.Add(selected_seg_geom);
-                    var merged_geoms = GeometryEngine.Instance.Union(_selectedSegments[0], selected_seg_geom) as Polyline;
-                    lock (_lock)
-                    {
-                        this.UpdateOverlay(_graphic, merged_geoms, _lineSymbol.MakeSymbolReference());
-                    }
-
-                    // Deubug output int point
-                    var seg1 = _selectedSegments[0].Parts.FirstOrDefault().FirstOrDefault();
-                    var seg2 = _selectedSegments[1].Parts.FirstOrDefault().FirstOrDefault();
-                    var test_line = 
-                    GeometryEngine.Instance.Union(
-                        PolylineBuilderEx.CreatePolyline(new[] { seg1.EndPoint, int_point }, selected_segment.SpatialReference),
-                        PolylineBuilderEx.CreatePolyline(new[] { seg2.EndPoint, int_point }, selected_segment.SpatialReference)
-                    );
-                    insp.Shape = test_line;
-
-                    lock (_lock)
-                    {
-                        if (_graphic != null)
-                            _graphic.Dispose();
-
-                        _graphic = this.AddOverlay(test_line, _lineSymbol.MakeSymbolReference());
-                    }
-
                 }
 
                 // Edit operation syntax
@@ -152,7 +147,7 @@ namespace Chamfer
         protected override Task<bool> OnToolDeactivateAsync(bool hasMapViewChanged)
         {
             _lineSymbol = null;
-            _selectedSegments = new();
+            _selected_segments = new();
 
             lock (_lock)
             {
@@ -176,15 +171,11 @@ namespace Chamfer
         }
 
         // Intended to operate on two polylines which each only contain one segment
-        private static MapPoint ChamferLines(Polyline line1, Polyline line2, float length=0)
+        private static MapPoint GetIntersectionPoint(Polyline line1, Polyline line2)
         {
-
+            // TODO: Add case for existing intersection point
             if (line1.SpatialReference != line2.SpatialReference)
                 return null;
-
-            // TODO: case if lines actually intersect
-            //MapPoint intersect_point = GeometryEngine.Instance.Intersection(line1, line2, GeometryDimensionType.EsriGeometry0Dimension) as MapPoint;
-
             // Find theoretical intersection point between two segments (assumes straight line)
             // TODO: add case for curved segments (tangent line @ endpoint?)
             List<double?> slopes = new();
@@ -201,14 +192,12 @@ namespace Chamfer
                     : line_segment.StartCoordinate.Y - (slope.Value * line_segment.StartCoordinate.X)
                 );
             }
-
             double int_x;
             double int_y;
-
-            // Case: parallel lines (also catches parallel vertical lines)
+            // Case: Parallel lines (also catches parallel vertical lines)
             if (slopes[0] == slopes[1])
                 return null;
-            // Case: one vertical line
+            // Case: One vertical line
             int null_idx = slopes.IndexOf(null);
             if (null_idx != -1)
             {
@@ -216,7 +205,7 @@ namespace Chamfer
                 int_x = intercepts[null_idx];
                 int_y = (slopes[non_null_idx].Value * int_x) + intercepts[non_null_idx];
             }
-            // Case: no vertical lines
+            // Case: No vertical lines
             else
             {
                 int_x = (intercepts[1] - intercepts[0]) / (slopes[0].Value - slopes[1].Value);
@@ -226,6 +215,26 @@ namespace Chamfer
             MapPoint int_point = MapPointBuilderEx.CreateMapPoint(int_x, int_y, line1.SpatialReference);
 
             return int_point;
+        }
+
+        // Intended to operate on two polylines which each only contain one segment
+        private static Polyline ChamferLines(Polyline line1, Polyline line2, double length = 0)
+        {
+            MapPoint intersection_point = GetIntersectionPoint(line1, line2);
+
+            if (intersection_point == null)
+                return null;
+
+            List<Polyline> theoretical_extensions = new();
+            foreach (Polyline line in new[] { line1, line2 })
+            {
+                MapPoint closest_point = line.Points
+                    .OrderBy(point => GeometryEngine.Instance.Distance(intersection_point, point))
+                    .FirstOrDefault();
+                theoretical_extensions.Add(PolylineBuilderEx.CreatePolyline(new[] { closest_point, intersection_point }, line.SpatialReference));
+            }
+
+            return GeometryEngine.Instance.Union(theoretical_extensions[0], theoretical_extensions[1]) as Polyline;
         }
 
         #endregion
