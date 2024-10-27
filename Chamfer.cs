75d3dde97512bc0e79d3d0f25c0359146afdb19f
DanielRubianes <DanielRubianes1352@gmail.com>
2024-10-27 17:41:38 -0400

    Many attempts to figure out average line/angle

--- Chamfer.cs
+++ Chamfer.cs
@@ -11,6 +11,7 @@ using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Contracts;
 using ArcGIS.Desktop.Framework.Dialogs;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Internal.Core;
 using ArcGIS.Desktop.KnowledgeGraph;
 using ArcGIS.Desktop.Layouts;
 using ArcGIS.Desktop.Mapping;
@@ -19,9 +20,12 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using static System.Formats.Asn1.AsnWriter;
 using Geometry = ArcGIS.Core.Geometry.Geometry;
 
 namespace Chamfer
@@ -50,12 +54,31 @@ namespace Chamfer
         public class InfiniteLine
         // Contains a slope-intercept defenition of a two-point line, as well as the endpoints used to construct said line
         // Intended to operate on two-point line segments
+        // TODO: Implement double.PositiveInfinity rather than null railroad
         {
             public readonly Polyline DisplayGeometry;
             public readonly MapPoint StartPoint;
             public readonly MapPoint EndPoint;
             public readonly double? Slope;
             public readonly double Intercept;
+
+            public static InfiniteLine Average(InfiniteLine line1, InfiniteLine line2)
+            {
+                var x_component = ((line1.EndPoint.X - line1.StartPoint.X) + (line2.EndPoint.X - line2.StartPoint.X)) / 2;
+                var y_component = ((line1.EndPoint.Y - line1.StartPoint.Y) + (line2.EndPoint.Y - line2.StartPoint.Y)) / 2;
+                return new InfiniteLine
+                (
+                    MapPointBuilderEx.CreateMapPoint(0, 0, line1.StartPoint.SpatialReference),
+                    MapPointBuilderEx.CreateMapPoint(x_component, y_component, line1.StartPoint.SpatialReference)
+                );
+            }
+
+            public static InfiniteLine rotate90(InfiniteLine line)
+            {
+                var x_component = -1 * (line.EndPoint.X - line.StartPoint.X);
+                var y_component = (line.EndPoint.Y - line.StartPoint.Y);
+                return new InfiniteLine(line.StartPoint, MapPointBuilderEx.CreateMapPoint(x_component, y_component, line.EndPoint.SpatialReference));
+            }
 
             public InfiniteLine(Polyline line) : this(line.Points[0], line.Points[^1], line) { }
             public InfiniteLine(Segment seg) : this(seg.StartPoint, seg.EndPoint) { }
@@ -293,8 +316,8 @@ namespace Chamfer
             // Case: One vertical line
             if (line1.Slope == null || line2.Slope == null)
             {
-                InfiniteLine vertical_line = line1 == null ? line1 : line2;
-                InfiniteLine non_vertical_line = line2 == null ? line1 : line1;
+                InfiniteLine vertical_line = (line1.Slope == null) ? line1 : line2;
+                InfiniteLine non_vertical_line = (line2.Slope == null) ? line1 : line1;
                 int_x = vertical_line.Intercept;
                 int_y = (non_vertical_line.Slope.Value * int_x) + non_vertical_line.Intercept;
             }
@@ -314,7 +337,7 @@ namespace Chamfer
         private static Polyline ChamferLines(InfiniteLine line1, InfiniteLine line2, MapPoint mouse_point = null)
         {
             MapPoint intersection_point = GetIntersectionPoint(line1, line2);
-            if (intersection_point == null)
+            if (intersection_point == null) // This will filter out parallel lines, including two with null slope
                 return null;
             List<Polyline> theoretical_extensions = new();
             foreach (InfiniteLine line in new[] { line1, line2 })
@@ -325,7 +348,26 @@ namespace Chamfer
                 theoretical_extensions.Add(PolylineBuilderEx.CreatePolyline(new[] { closest_point, intersection_point }, line.StartPoint.SpatialReference));
             }
             if (mouse_point != null)
-                theoretical_extensions[1] = PolylineBuilderEx.CreatePolyline(new[] { mouse_point, intersection_point }, intersection_point.SpatialReference);
+            {
+                //double? avg_slope = (line1.Slope == null) ? line2.Slope
+                //    : (line2.Slope == null) ? line1.Slope
+                //    : (line1.Slope.Value + line2.Slope.Value) / 2;
+
+                //double angle1 = line1.Slope.HasValue ? Math.Atan(line1.Slope.Value) : Math.PI / 2;
+                //double angle2 = line2.Slope.HasValue ? Math.Atan(line2.Slope.Value) : Math.PI / 2;
+
+                //double avg_angle = (angle1 / angle2) / 2;
+
+                //double avg_slope = Math.Tan(avg_angle);
+
+                //MapPoint translated_mouse_point = GeometryEngine.Instance.Move(mouse_point, 1, avg_slope) as MapPoint;
+                //InfiniteLine mouse_line = new InfiniteLine(mouse_point, translated_mouse_point);
+
+                var mouse_line = InfiniteLine.rotate90(line1);
+                var int_pt1 = GetIntersectionPoint(line1, mouse_line);
+                var int_pt2 = GetIntersectionPoint(line2, mouse_line);
+                return PolylineBuilderEx.CreatePolyline(new[] { int_pt1, int_pt2 }, intersection_point.SpatialReference);
+            }
             return GeometryEngine.Instance.Union(new[] { theoretical_extensions[0], theoretical_extensions[1] }) as Polyline;
         }
 
