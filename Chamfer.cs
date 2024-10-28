a3735352b322d75d2285aaaa8f5b5a6664f871a6
Daniel Rubianes <danielr1352@gmail.com>
2024-10-28 07:56:16 -0400

    Project selected feature to mapspace coordinate system

--- Chamfer.cs
+++ Chamfer.cs
@@ -136,11 +136,12 @@ namespace Chamfer
 
                 if (outline_geom == null) // Exit if null or non-line geometry
                     return false;
-                
+
                 // Find closest segment (polyline part) to the selection point
-                Segment selected_segment = outline_geom.Parts
+                Polyline projected_selection = GeometryEngine.Instance.Project(outline_geom, point_selection.SpatialReference) as Polyline;
+                Segment selected_segment = projected_selection.Parts
                     .SelectMany(segment => segment).ToList()
-                    .OrderBy(segment => GeometryEngine.Instance.Distance(point_selection, PolylineBuilderEx.CreatePolyline(segment)))
+                    .OrderBy(segment => GeometryEngine.Instance.Distance(point_selection, PolylineBuilderEx.CreatePolyline(segment, segment.SpatialReference)))
                     .FirstOrDefault();
                 InfiniteLine selected_line = new InfiniteLine(selected_segment);
 
